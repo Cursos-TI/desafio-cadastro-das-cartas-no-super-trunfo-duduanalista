3a31d9be7cddd5d46b3fae44a2b8219d3e19d8e8
duduanalista <edy_potter@hotmail.com>
2025-02-06 00:18:16 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_CARTAS 32 // 8 estados * 4 cidades
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
+int main() {
+    char codigo[4] = 1 ; // codigo da carta
+    char nome[50] = "Goiania" ;// nome da cidade
+    int populacao = 1.000 ; // população da cidade
+    float area = 3000 ; // Área total em km²
+    float pib; 500 // PIB da cidade
+    int pontosTuristicos = 7 ; // Número de pontos turísticos
+} Carta ;
+
+void cadastrarCarta(carta cartas[],
+int *quantidade) {
+    if (*quantidade >= MAX_CARTAS){
+          printf("Limite de cartas atingido! \n");
+        return;
+    }
+
+    Carta nova;
+    printf("Digite o código da cidade(Ex: A01, B02): ");
+    scanf("%s, nova.codigo");
+
+    printf("Digite o nome da cidade:");
+    scanf(" %[^\n]", nova.nome);
+
+    printf("Digite a população da cidade: ");
+    scanf("%d", &nova.populacao);
+
+    printf("Digite a área da cidade em km²: ");
+    scanf("%f, &nova.area");
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &nova.pontosTuristicos);
+
+        cartas[*quantidade] = nova;
+        (*quantidade)++;
+        printf("Carta cadastrada com sucesso!\n");
+
+}
+
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
